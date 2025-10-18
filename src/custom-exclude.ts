1a001c897717051afeff7dbf3e5a63324ce36766
pranta-barua007 <prantabarua247@gmail.com>
2025-10-18 22:13:13 +0600

    feat: custom exclude

---
+++ src/custom-exclude.ts
@@ -0,0 +1,11 @@
+import { Expect, Equal } from "./type-utils";
+
+type MyExclude<T, U> = T extends U ? never : T
+
+// Test cases
+
+type cases = [
+    Expect<Equal<MyExclude<"a" | "b" | "c" , "a">, "b" | "c">>,
+    Expect<Equal<MyExclude<"a" | "b" | "c" , "a" | "b">, "c">>,
+    Expect<Equal<MyExclude<string | number | (() => void), Function>, string | number>>
+]
