4b9bc4361eceef6268ff1455b4f547ae9b591b00
pranta-barua007 <prantabarua247@gmail.com>
2025-10-18 22:28:22 +0600

    feat: pascal or camecase to kebab-case progress, but not passing all, there is a problem a - is coming before

---
+++ src/kabab-case.ts
@@ -0,0 +1,25 @@
+// Task 
+
+// Replace camelCase or PascalCase string with kebab-case
+
+// `FooBarBaz` -> `foo-bar-baz`
+
+
+type KebabCase<S extends string> = S extends `${infer A}${infer Rest}` ? (
+    A extends Uppercase<A> 
+        ? `-${Lowercase<A>}${KebabCase<Rest>}` 
+        : `${A}${KebabCase<Rest>}`
+) : S
+
+import type { Equal, Expect } from "./type-utils"
+
+type cases = [
+    Expect<Equal<KebabCase<"FooBarBaz">, "foo-bar-baz">>,
+    Expect<Equal<KebabCase<"fooBarBaz">, "foo-bar-baz">>,
+    Expect<Equal<KebabCase<"fooBARBaz">, "foo-b-a-r-baz">>,
+    Expect<Equal<KebabCase<"foo_bar">, "foo_bar">>,
+    Expect<Equal<KebabCase<"ABC">, "a-b-c">>,
+    Expect<Equal<KebabCase<"-">, "-">>,
+    Expect<Equal<KebabCase<"">, "">>,
+    Expect<Equal<KebabCase<"😎">, "😎">>
+]
