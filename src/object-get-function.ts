d17e2de4af4f39c070e4697839f87a27beba0b97
pranta-barua007 <prantabarua247@gmail.com>
2025-11-04 11:00:56 +0600

    feat: Get function implemented for deeply nested object

--- src/object-get-function.ts
+++ src/object-get-function.ts
@@ -2,7 +2,13 @@
 type Get<
     Obj, 
     Key extends string
-> = Key extends keyof Obj ? Obj[Key] : never
+> = Key extends keyof Obj 
+    ? Obj[Key]
+    : Key extends `${infer K1}.${infer Rest}`
+        ? K1 extends keyof Obj
+            ? Get<Obj[K1], Rest>
+            : never 
+        : never
 
 //  ---- Test Cases ----
 import type { Equal, Expect } from "./type-utils"
@@ -13,7 +19,8 @@ type cases = [
     Expect<Equal<Get<Data, "foo.include">, true>>,
     Expect<Equal<Get<Data, "foo.baz">, false>>,
     Expect<Equal<Get<Data, "hello">, "world">>,
-    Expect<Equal<Get<Data, "no.existent">, never>>
+    Expect<Equal<Get<Data, "no.existent">, never>>,
+    Expect<Equal<Get<DeeplyNested, "a.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p.q.r.s.t.value">, 42>>,
 ]
 
 type Data = {
@@ -26,4 +33,48 @@ type Data = {
     }
     "foo.baz": false,
     hello: "world"
+}
+
+type DeeplyNested = {
+    a: {
+        b: {
+            c: {
+                d: {
+                    e: {
+                        f: {
+                            g: {
+                                h: {
+                                    i: {
+                                        j: {
+                                            k: {
+                                                l: {
+                                                    m: {
+                                                        n: {
+                                                            o: {
+                                                                p: {
+                                                                    q: {
+                                                                        r: {
+                                                                            s: {
+                                                                                t: {
+                                                                                    value: 42
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
